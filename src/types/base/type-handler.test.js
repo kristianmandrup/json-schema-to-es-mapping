5cea9610e8ae4e440a3d7dbae25f71d4f03533f8
Kristian Mandrup <kmandrup@gmail.com>
2019-04-23 08:40:17 +0700

    TypeHandler tests

---
+++ src/types/base/type-handler.test.js
@@ -0,0 +1,47 @@
+const { createTypeHandler } = require("./type-handler");
+
+const create = createTypeHandler;
+
+const Project = {
+  type: "object",
+  properties: {
+    title: {
+      type: "string"
+    }
+  }
+};
+const schema = {
+  definitions: {
+    Project
+  }
+};
+
+describe("TypeHandler", () => {
+  const config = {
+    typeMap: {
+      string: "keyword"
+    }
+  };
+  const typeName = "string";
+  const entry = {
+    type: "string"
+  };
+  const handler = create({ typeName, entry }, config);
+  describe("metaType", () => {
+    test("string", () => {
+      expect(handler.metaType).toEqual("string");
+    });
+  });
+
+  describe("metaType", () => {
+    test("keyword", () => {
+      expect(handler.metaType).toEqual("keyword");
+    });
+  });
+
+  describe("type", () => {
+    test("string", () => {
+      expect(handler.type).toEqual("string");
+    });
+  });
+});
