a3b24d54a2aabe531251ccc64d20e22b6dd06ac7
Kristian Mandrup <kmandrup@gmail.com>
2019-04-29 09:18:36 +0700

    init tests WIP

--- src/types/base/index.test.js
+++ src/types/base/index.test.js
@@ -29,31 +29,96 @@ describe("MappingBaseType", () => {
     describe("create keyMaker", () => {
       describe("default", () => {
         const mapper = create(schema);
-        test("is function", () => {
-          expect(typeof mapper.keyMaker).toEqual("function");
+        test("is undefined before init", () => {
+          expect(mapper.keyMaker).toBeUndefined();
         });
 
-        test("generates key as expected", () => {
-          const opts = { key: mapper.key, parentName: mapper.parentName };
-          expect(mapper.keyMaker(opts, mapper.config)).toEqual("x");
+        describe("initKeyMaker", () => {
+          mapper.initKeyMaker();
+          test("is function", () => {
+            expect(typeof mapper.keyMaker).toEqual("function");
+          });
+
+          test("generates key as expected", () => {
+            const opts = { key: mapper.key, parentName: mapper.parentName };
+            expect(mapper.keyMaker(opts, mapper.config)).toEqual("x");
+          });
         });
       });
       describe("passed in config", () => {});
     });
     describe("create entryObj", () => {
-      describe("default", () => {});
+      describe("default", () => {
+        const mapper = create(schema);
+        test("is undefined before init", () => {
+          expect(mapper.entryObj).toBeUndefined();
+        });
+
+        describe("initEntryObj", () => {
+          mapper.initEntryObj();
+
+          test("is function", () => {
+            expect(typeof mapper.entryObj).toEqual("function");
+          });
+        });
+      });
       describe("passed in config", () => {});
     });
     describe("create dispatcher", () => {
-      describe("default", () => {});
+      describe("default", () => {
+        const mapper = create(schema);
+        test("is undefined before init", () => {
+          expect(mapper.dispatcher).toBeUndefined();
+        });
+
+        describe("initDispatcher", () => {
+          mapper.initDispatcher();
+
+          test("is function", () => {
+            expect(typeof mapper.dispatcher).toEqual("function");
+          });
+        });
+      });
       describe("passed in config", () => {});
     });
     describe("create typeHandler", () => {
-      describe("default", () => {});
+      describe("default", () => {
+        const mapper = create(schema);
+        test("is undefined before init", () => {
+          expect(mapper.typeHandler).toBeUndefined();
+        });
+
+        describe("initTypeHandler", () => {
+          mapper.initTypeHandler();
+
+          test("is function", () => {
+            expect(typeof mapper.typeHandler).toEqual("function");
+          });
+        });
+      });
       describe("passed in config", () => {});
     });
     describe("create resultHandler", () => {
-      describe("default", () => {});
+      describe("default", () => {
+        const mapper = create(schema);
+        test("is undefined before init", () => {
+          expect(mapper.resultHandler).toBeUndefined();
+        });
+
+        describe("initResultHandler", () => {
+          test("throws if no keyMaker", () => {
+            expect(mapper.initResultHandler()).toThrow();
+          });
+
+          describe("initialized", () => {
+            mapper.initResultHandler();
+
+            test("is function", () => {
+              expect(typeof mapper.resultHandler).toEqual("function");
+            });
+          });
+        });
+      });
       describe("passed in config", () => {});
     });
     describe("create referenceResolver", () => {
