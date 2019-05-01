994b29c24ad0b94d2cbcc4c85a1ae22aac91dd3a
Kristian Mandrup <kmandrup@gmail.com>
2019-05-01 08:28:37 +0700

    all xgodon tests converted

--- test/xgodon/samples.test.js
+++ test/xgodon/samples.test.js
@@ -7,7 +7,10 @@ const {
   a,
   b,
   c,
-  d
+  d,
+  e,
+  f,
+  g
 } = require("./samples");
 const JsToEs = build;
 
@@ -74,6 +77,87 @@ test("list_to_str", () => {
     MedlineCitation: {
       type: "text",
       fields: { keyword: { type: "keyword", ignore_above: 256 } }
+    }
+  });
+});
+
+test("list_to_float", () => {
+  const { properties } = JsToEs(c);
+  expect(properties).toEqual({
+    MedlineCitation: {
+      type: "float"
+    }
+  });
+});
+
+test("list_to_int", () => {
+  const { properties } = JsToEs(d);
+  expect(properties).toEqual({
+    MedlineCitation: {
+      type: "integer"
+    }
+  });
+});
+
+test("anyof_str_obj", () => {
+  const { properties } = JsToEs(e);
+  expect(properties).toEqual({
+    MedlineCitation: {
+      type: "nested",
+      properties: {
+        object_from_string: {
+          type: "text",
+          fields: { keyword: { type: "keyword", ignore_above: 256 } }
+        },
+        prop1: {
+          type: "text",
+          fields: { keyword: { type: "keyword", ignore_above: 256 } }
+        },
+        prop2: {
+          type: "text",
+          fields: { keyword: { type: "keyword", ignore_above: 256 } }
+        }
+      }
+    }
+  });
+});
+
+test("anyof_num_obj", () => {
+  const { properties } = JsToEs(f);
+  expect(properties).toEqual({
+    MedlineCitation: {
+      type: "nested",
+      properties: {
+        object_from_number: { type: "float" },
+        prop1: {
+          type: "text",
+          fields: { keyword: { type: "keyword", ignore_above: 256 } }
+        },
+        prop2: {
+          type: "text",
+          fields: { keyword: { type: "keyword", ignore_above: 256 } }
+        }
+      }
+    }
+  });
+});
+
+test("anyof_int_obj", () => {
+  const { properties } = JsToEs(g);
+  expect(properties).toEqual({
+    MedlineCitation: {
+      type: "nested",
+      properties: {
+        object_from_integer: { type: "integer" },
+        prop1: {
+          type: "text",
+          fields: { keyword: { type: "keyword", ignore_above: 256 } }
+        },
+        prop2: {
+          type: "text",
+          fields: { keyword: { type: "keyword", ignore_above: 256 } }
+        }
+      }
     }
   });
 });
